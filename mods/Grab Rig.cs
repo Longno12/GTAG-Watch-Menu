7b31146eb7376b889f09c0f6dcb572e516788cde
Longno12 <157077978+Longno12@users.noreply.github.com>
2025-10-13 00:04:57 +0100

    Update Grab Rig.cs

--- mods/Grab Rig.cs
+++ mods/Grab Rig.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using static QRCoder.PayloadGenerator;
 
 namespace Watch_Menu.mods
 {
     internal class Grab_Rig
     {
+        public static bool Exceptions = false;
+        public static bool stuff = false;
+        public static float laggyDelay;
+        public static void LaggyRig()
+        {
+            Exceptions = true;
+            if (Time.time > laggyDelay)
+            {
+                GorillaTagger.Instance.offlineVRRig.enabled = true;
+                stuff = true;
+                laggyDelay = Time.time + 0.5f;
+            }
+            else
+            {
+                if (stuff)
+                {
+                    stuff = false;
+                }
+                else
+                {
+                    GorillaTagger.Instance.offlineVRRig.enabled = false;
+                }
+            }
+        }
     }
 }
