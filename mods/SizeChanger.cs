e7909df83b880944e08c289121b55f57cb376143
Longno12 <157077978+Longno12@users.noreply.github.com>
2025-10-15 08:47:59 +0100

    Add files via upload

---
+++ mods/SizeChanger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Watch_Menu.mods
+{
+    internal class Size_Changer
+    {
+        public static void SizeChanger()
+        {
+            float sizeScale = GorillaTagger.Instance.offlineVRRig.NativeScale;
+            float increment = 0.1f;
+            if (ControllerInputPoller.instance.leftGrab)
+            {
+                sizeScale -= increment;
+            }
+            if (ControllerInputPoller.instance.rightGrab)
+            {
+                sizeScale += increment;
+            }
+            if (sizeScale < 0.05f)
+            {
+                sizeScale = 0.05f;
+            }
+            GorillaTagger.Instance.offlineVRRig.transform.localScale = Vector3.one * sizeScale;
+            GorillaTagger.Instance.offlineVRRig.NativeScale = sizeScale;
+            typeof(GorillaLocomotion.GTPlayer).GetField("nativeScale", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(GorillaLocomotion.GTPlayer.Instance, sizeScale);
+        }
+    }
+}
